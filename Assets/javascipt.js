e55d282cc9fa3c9a1a5149eb526e6acb4d72af6b
Brenton Weaver <brenton.weaver@gmail.com>
2022-01-03 00:57:03 +1100

    added textarea to the html to try to figure out the input

--- Assets/javascipt.js
+++ Assets/javascipt.js
@@ -5,9 +5,18 @@ var currentDayMJS = moment().format("dddd, MMMM Do YYYY");
 setInterval(pushTime, 1000);
 
 function pushTime() {
-    var tempTimeMJS= moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    var tempTimeMJS= moment().format("dddd, Do MMMM YYYY, h:mm:ss a");
     currentDay.innerHTML = ("Today is " + tempTimeMJS); 
 }
 
 
 
+function timerRecolor(){
+    var hourMJS = moment().format("H")
+    var background = document.getElementById("plannerBox");
+
+    console.log(hourMJS)
+    console.log(background)
+}
+
+timerRecolor();
